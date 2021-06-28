4b45ea76795d147aca44679ca89c8d93aef4c7f1
Taylor Martins <taylorfmartins@live.com>
2021-06-27 22:37:11 -0300

    Funcionários
    Método de adição e listagem dos funcionários

---
+++ SASF.WebApp/Controllers/FuncionarioController.cs
@@ -0,0 +1,51 @@
+﻿using Homeland.SASF.Model;
+using Homeland.SASF.Persistencia;
+using Homeland.SASF.WebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Homeland.SASF.WebApp.Controllers
+{
+    public class FuncionarioController : Controller
+    {
+        private readonly IRepository<Funcionario> _repo;
+
+        public FuncionarioController(IRepository<Funcionario> repository)
+        {
+            _repo = repository;
+        }
+        private IEnumerable<Funcionario> Carregar()
+        {
+            return _repo.All
+                .ToList();
+        }
+
+        public IActionResult Index()
+        {
+            var model = new FuncionarioViewModel
+            {
+                Funcionarios = Carregar()
+            };
+
+            return View(model);
+        }
+
+        public IActionResult Novo()
+        {
+            return View();
+        }
+
+        public IActionResult Adicionar(Funcionario model)
+        {
+            if (ModelState.IsValid)
+            {
+                _repo.Add(model);
+                return RedirectToAction("Index", "Funcionario");
+            }
+            return View(model);
+        }
+    }
+}
---
+++ SASF.WebApp/Models/FuncionarioViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Homeland.SASF.Model;
+using System.Collections.Generic;
+
+namespace Homeland.SASF.WebApp.Models
+{
+    public class FuncionarioViewModel
+    {
+        public IEnumerable<Funcionario> Funcionarios { get; set; }
+    }
+}
