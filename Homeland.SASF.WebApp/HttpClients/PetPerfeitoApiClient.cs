d271d877f750bf82c116a692d415b73658da1f37
Taylor Martins <taylorfmartins@live.com>
2021-07-10 13:46:22 -0300

    Remocao RestSharp
    Package desnecessario

--- Homeland.SASF.WebApp/HttpClients/PetPerfeitoApiClient.cs
+++ Homeland.SASF.WebApp/HttpClients/PetPerfeitoApiClient.cs
@@ -1,6 +1,4 @@
 ﻿using Homeland.SASF.Model;
-using Newtonsoft.Json;
-using RestSharp;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -26,19 +24,6 @@ namespace Homeland.SASF.WebApp.HttpClients
             HttpResponseMessage response = await _httpClient.GetAsync("");
             response.EnsureSuccessStatusCode();
             return await response.Content.ReadAsAsync<List<PetPerfeitoApi>>();
-        }
-
-
-        public List<PetPerfeitoApi> GetPetsRestSharp()
-        {
-            var client = new RestClient("http://petperfeito.kinghost.net/view/api.php?senha=e23e434r5443e33ee3e22");
-            client.Timeout = -1;
-            var request = new RestRequest(Method.GET);
-            request.AddHeader("Content", "application/json");
-            request.AddHeader("Cookie", "PHPSESSID=376224c1074d781eb01b1bf9ab46c2cc");
-            IRestResponse response = client.Execute(request);
-            PetRestCharp petRestCharp = JsonConvert.DeserializeObject<PetRestCharp>(response.Content);
-            return petRestCharp.pets.ToList();
         }
     }
 }
