494ffc94b7b05c03b6b7d0cb45d290531fdc4cbd
Taylor Martins <taylorfmartins@live.com>
2021-07-14 01:11:52 -0300

    Ocorrencia
    Questionario diario, index para consulta e autorizacoes de acesso ao formulario.

---
+++ Homeland.SASF.Model/Ocorrencia.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Homeland.SASF.Model
+{
+    public class Ocorrencia
+    {
+        public int numero { get; set; }
+        public int matricula { get; set; }
+        public DateTime data { get; set; }
+        public string respostaEstado { get; set; }
+        public string respostaSintomas { get; set; }
+        public string respostaVacina { get; set; }
+        public bool solicitarHistorico { get; set; }
+    }
+}
---
+++ Homeland.SASF.WebApp/Controllers/OcorrenciaController.cs
@@ -0,0 +1,54 @@
+﻿using Homeland.SASF.Model;
+using Homeland.SASF.Persistencia;
+using Homeland.SASF.WebApp.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Homeland.SASF.WebApp.Controllers
+{
+    public class OcorrenciaController : Controller
+    {
+        private readonly IRepository<Ocorrencia> _repo;
+
+        public OcorrenciaController(IRepository<Ocorrencia> repository)
+        {
+            _repo = repository;
+        }
+
+        private IEnumerable<Ocorrencia> Carregar()
+        {
+            return _repo.All
+                .ToList();
+        }
+
+        [Authorize]
+        public IActionResult Index()
+        {
+            var model = new OcorrenciaViewModel
+            {
+                Ocorrencias = Carregar()
+            };
+
+            return View(model);
+        }
+
+        public IActionResult Adicionar(Ocorrencia model)
+        {
+            if (ModelState.IsValid)
+            {
+                model.data = DateTime.Now;
+                _repo.Add(model);
+                return RedirectToAction("Index", "Home");
+            }
+            return View(model);
+        }
+
+        public IActionResult Novo()
+        {
+            return View();
+        }
+    }
+}
